7579973d88f492ab1b5464f5e5799c1d63cdf6f1
wesmith <smiwarsky@gmail.com>
2020-12-15 00:15:51 -0700

    python3 changes, added 5.3 compute capability for nano

--- Chapter03/deviceQuery.py
+++ Chapter03/deviceQuery.py
@@ -2,24 +2,24 @@ import pycuda
 import pycuda.driver as drv
 drv.init()
 
-print 'CUDA device query (PyCUDA version) \n'
+print('CUDA device query (PyCUDA version) \n') # WS mod python3
 
-print 'Detected {} CUDA Capable device(s) \n'.format(drv.Device.count())
+print('Detected {} CUDA Capable device(s) \n'.format(drv.Device.count())) # WS mod
 
 for i in range(drv.Device.count()):
     
     gpu_device = drv.Device(i)
-    print 'Device {}: {}'.format( i, gpu_device.name() ) 
+    print('Device {}: {}'.format( i, gpu_device.name() )) # WS mod
     compute_capability = float( '%d.%d' % gpu_device.compute_capability() )
-    print '\t Compute Capability: {}'.format(compute_capability)
-    print '\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2))
+    print('\t Compute Capability: {}'.format(compute_capability)) # WS mod
+    print('\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2))) # WS mod
     
     # The following will give us all remaining device attributes as seen 
     # in the original deviceQuery.
     # We set up a dictionary as such so that we can easily index
     # the values using a string descriptor.
     
-    device_attributes_tuples = gpu_device.get_attributes().iteritems() 
+    device_attributes_tuples = gpu_device.get_attributes().items()  # WS mod python3
     device_attributes = {}
     
     for k, v in device_attributes_tuples:
@@ -32,11 +32,13 @@ for i in range(drv.Device.count()):
     # See the following:
     # http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capabilities
     
-    cuda_cores_per_mp = { 5.0 : 128, 5.1 : 128, 5.2 : 128, 6.0 : 64, 6.1 : 128, 6.2 : 128}[compute_capability]
+    cuda_cores_per_mp = { 5.3 : 128, 5.1 : 128, 5.2 : 128, 6.0 : 64, 6.1 : 128, 6.2 : 128} \
+        [compute_capability]  # WS mod added 5.3
     
-    print '\t ({}) Multiprocessors, ({}) CUDA Cores / Multiprocessor: {} CUDA Cores'.format(num_mp, cuda_cores_per_mp, num_mp*cuda_cores_per_mp)
+    print('\t ({}) Multiprocessors, ({}) CUDA Cores / Multiprocessor: {} CUDA Cores'\
+          .format(num_mp, cuda_cores_per_mp, num_mp*cuda_cores_per_mp)) # WS mod
     
     device_attributes.pop('MULTIPROCESSOR_COUNT')
     
     for k in device_attributes.keys():
-        print '\t {}: {}'.format(k, device_attributes[k])
+        print('\t {}: {}'.format(k, device_attributes[k])) # WS mod
