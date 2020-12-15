4bcda108c138cec6409cc517f457dcc5f43a462e
wesmith <smiwarsky@gmail.com>
2020-12-15 05:14:36 -0700

    mods for python3

--- Chapter03/simple_scankernel0.py
+++ Chapter03/simple_scankernel0.py
@@ -6,5 +6,5 @@ from pycuda.scan import InclusiveScanKernel
 seq = np.array([1,2,3,4],dtype=np.int32)
 seq_gpu = gpuarray.to_gpu(seq)
 sum_gpu = InclusiveScanKernel(np.int32, "a+b")
-print sum_gpu(seq_gpu).get()
-print np.cumsum(seq)
+print(sum_gpu(seq_gpu).get())  # WS mod
+print(np.cumsum(seq))  # WS mod
--- Chapter03/simple_scankernel1.py
+++ Chapter03/simple_scankernel1.py
@@ -3,8 +3,8 @@ import pycuda.autoinit
 from pycuda import gpuarray
 from pycuda.scan import InclusiveScanKernel
 
-seq = np.array([1,100,-3,-10000, 4, 10000, 66, 14, 21],dtype=np.int32)
+seq = np.array([1,100,-3,-10000, 4, 10000, 66, 14, 21], dtype=np.int32)
 seq_gpu = gpuarray.to_gpu(seq)
 max_gpu = InclusiveScanKernel(np.int32, "a > b ? a : b")
-print max_gpu(seq_gpu).get()[-1]
-print np.max(seq)
+print(max_gpu(seq_gpu).get()[-1])  # WS mod
+print(np.max(seq))  # WS mod
