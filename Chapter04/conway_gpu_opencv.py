dd9f9157e458693a36c0b1871c3652c6bf47b659
wesmith <smiwarsky@gmail.com>
2020-12-19 02:18:26 -0700

    added pval, added capability to see first seed frame

--- Chapter04/conway_gpu_opencv.py
+++ Chapter04/conway_gpu_opencv.py
@@ -74,10 +74,11 @@ def update_gpu(newLattice_gpu, lattice_gpu, N):  # WS rewrite for openCV display
 
 if __name__ == '__main__':
     # set lattice size
-    N = 128 * 4      # 768x768 starts to slow down a little
-    window_scale = 1 # scaling the window slows down the display a little
+    N = 128 * 5      # > 768x768 starts to slow down a little: bottlenecked by transfer of gpu mem to cpu?
+    window_scale = 1 # scaling the window > 1 slows down the display a little for large N
+    pval = 0.25 # WS mod: probability of 1's (0.25 is nominal value)
 
-    lattice     = np.int32(np.random.choice([1,0], N*N, p=[0.25, 0.75]).reshape(N, N))
+    lattice     = np.int32(np.random.choice([1,0], N*N, p=[pval, 1-pval]).reshape(N, N))
     lattice_gpu = gpuarray.to_gpu(lattice)
 
     newLattice_gpu = gpuarray.empty_like(lattice_gpu) 
@@ -85,6 +86,13 @@ if __name__ == '__main__':
     txt = 'Game of Life {} x {} (press q to quit)'.format(N, N)
     cv2.namedWindow(txt, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(txt, window_scale*N, window_scale*N)
+
+    # WS mod: flash up first frame
+    dd = (255 * lattice).astype(np.uint8)
+    cv2.imshow(txt, dd)
+    cv2.moveWindow(txt, 0, 0)
+    print('Starting frame: press any key to continue, then "q" to finally quit')
+    cv2.waitKey(0)
 
     while True:  # couldn't get 'for' loop to work: 'while' loop works with waitKey
 
